f03e5b07930861c72021beaa629d2ca0716227fa
John Vo <vochidung243@gmail.com>
2025-02-20 07:38:43 +0700

    John updated

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -19,11 +19,11 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: 'postgres',  // Sử dụng PostgreSQL
-  host: process.env.DB_HOST || 'localhost',  // Địa chỉ host cơ sở dữ liệu
+  host: process.env.DB_HOST || 'dpg-cur7gi9opnds73c51mjg-a',  // Địa chỉ host cơ sở dữ liệu
   port: Number(process.env.DB_PORT) || 5432,  // Cổng kết nối cơ sở dữ liệu
-  username: process.env.DB_USERNAME || 'postgres',  // Tên người dùng
-  password: process.env.DB_PASSWORD || '000000',  // Mật khẩu
-  database: process.env.DB_NAME || 'gym_app',  // Tên cơ sở dữ liệu
+  username: process.env.DB_USERNAME || 'gym_app_dd48_user',  // Tên người dùng
+  password: process.env.DB_PASSWORD || 'oDf8rxEXm5XKNPRutLwV3NS94kKt00lY',  // Mật khẩu
+  database: process.env.DB_NAME || 'gym_app_dd48',  // Tên cơ sở dữ liệu
   synchronize: false,  // Tắt synchronize trong môi trường sản xuất
   migrations: ['src/migration/**/*.ts'],  // Đường dẫn đến migrations
   logging: false,  // Tắt logging SQL
